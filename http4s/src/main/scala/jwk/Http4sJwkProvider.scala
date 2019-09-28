fea71e71917a25dfefe3a4def4834428a57b6f63
Erlend Hamnaberg <erlend@hamnaberg.net>
2019-09-28 19:34:42 +0200

    Move entity decoder into http4s provider

--- http4s/src/main/scala/jwk/Http4sJwkProvider.scala
+++ http4s/src/main/scala/jwk/Http4sJwkProvider.scala
@@ -2,16 +2,20 @@ package jwk
 
 import java.net.URI
 
+import cats.effect.Sync
+import jwk.circe._
 import org.http4s.EntityDecoder
 import org.http4s.client.Client
 
-class Http4sJwkProvider[F[_]] private (client: Client[F])(implicit decoder: EntityDecoder[F, JwkSet]) extends JwkProvider[F] {
+class Http4sJwkProvider[F[_]: Sync] private(client: Client[F]) extends JwkProvider[F] {
+  implicit val decoder: EntityDecoder[F, JwkSet] = org.http4s.circe.jsonOf[F, JwkSet]
+
   override def load(uri: URI): F[JwkSet] = {
     client.expect[JwkSet](uri.toString)
   }
 }
 
 object Http4sJwkProvider {
-  def apply[F[_]](client: Client[F])(implicit decoder: EntityDecoder[F, JwkSet]): Http4sJwkProvider[F] =
+  def apply[F[_]: Sync](client: Client[F]): Http4sJwkProvider[F] =
     new Http4sJwkProvider[F](client)
 }
